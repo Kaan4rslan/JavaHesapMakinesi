1e5bfc39aaa04348145bcfa1cfd8a3aa4185c4d4
Kaan4rslan <155268464+Kaan4rslan@users.noreply.github.com>
2023-12-31 01:18:21 +0300

    Add files via upload

---
+++ HesapMakinesi/src/tr/net/eyal/kaanarslan/HesapMakinesi.java
@@ -0,0 +1,48 @@
+package tr.net.eyal.kaanarslan;
+
+import java.util.Scanner;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+
+        while (true) {
+            Scanner input = new Scanner(System.in);
+            System.out.println("birinci sayiyi giriniz: ");
+            int s1 = input.nextInt();
+
+            System.out.println("Ikinci sayiyi giriniz: ");
+            int s2 = input.nextInt();
+
+            System.out.println("Hangi islemi yapmak istiyorsunuz? (+) (-) (*) (/)");
+            String islem = input.next();
+
+
+            switch (islem) {
+                case "+":
+                    System.out.println(s1+s2);
+                    break;
+
+                case "-":
+                    System.out.println(s1-s2);
+                    break;
+
+                case "*":
+                    System.out.println(s1*s2);
+                    break;
+
+                case "/":
+                    System.out.println(s1/s2);
+                    break;
+
+                default:
+                    System.out.println("Bu islem bulunamadi");
+
+
+
+            }
+
+        }
+
+
+    }
+}
